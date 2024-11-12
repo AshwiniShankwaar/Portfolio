2ad621a85c278b96273fa0cedab7ea35c6ac05ac
ashwinishankwaar <ashwinishankwaar@gmail.com>
2024-11-13 00:44:28 +0530

    hero section updated and data added in json file

--- src/component/heroSection/HeroSection.jsx
+++ src/component/heroSection/HeroSection.jsx
@@ -1,28 +1,54 @@
-import style from './heroSection.module.css';
-import hero from '../../assets/hero/Designer.png';
-function HeroSection(){
-    return <section className={style.container}>
-        <div className={style.content}>
-            <h1 className={style.title}>
-                Hi, I'm Ashwini
-            </h1>
-            <p className={style.description}>
-                I'm a full-stack developer with 1 year industrial experience in Java and ReactJs. 
-            </p>
-            <a href='mailto:ashwinishankwaar@gmail.com' className={style.contactBtn}>
-                Contact Me 
-                <i className="fas fa-paper-plane"></i>
-                </a>
-        </div>
-        <img src={hero} alt="hero_image" className={style.heroImg}/>
-        <div className={style.topBlur}></div>
-        <div className={style.bottomBlur}></div>
-        <div id='socialMedia' className={style.social}>
-        <a href='https://www.instagram.com/ashwini_shankwaar/' target='_blank' rel="noopener noreferrer" className={style.instagram}><i class="fa-brands fa-instagram"></i></a>
-        <a href='https://www.linkedin.com/in/ashwini-kumar-3018a71b2/' target='_blank' rel="noopener noreferrer" className={style.linkedin}><i class="fa-brands fa-linkedin-in"></i></a>
-        <a href='https://github.com/AshwiniShankwaar' target='_blank' rel="noopener noreferrer" className={style.github}><i class="fa-brands fa-github"></i></a>
-        </div>
+import style from "./heroSection.module.css";
+import hero from "../../assets/hero/Designer.png";
+import heroSection from "../../data/heroSection.json";
+import resume from '../../data/resume.pdf';
+function HeroSection() {
+  const { name, description, mail_to, linkedin, instagram, github } =
+    heroSection[0];
+  return (
+    <section className={style.container}>
+      <div className={style.content}>
+        <h1 className={style.title}>Hi, I'm {name}</h1>
+        <p className={style.description}>{description}</p>
+        <div className={style.buttonSection}><a href={`mailto:${mail_to}`} className={style.contactBtn}>
+          Contact Me
+          <i className="fas fa-paper-plane"></i>
+        </a>
+        <a href={resume} download={`${name}_resume.pdf`} className={style.resume}>
+          <button className={style.button}>Get My Resume</button>
+        </a></div>
+      </div>
+      <img src={hero} alt="hero_image" className={style.heroImg} />
+      <div className={style.topBlur}></div>
+      <div className={style.bottomBlur}></div>
+      <div id="socialMedia" className={style.social}>
+        <a
+          href={instagram}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={style.instagram}
+        >
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a
+          href={linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={style.linkedin}
+        >
+          <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+        <a
+          href={github}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={style.github}
+        >
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </div>
     </section>
+  );
 }
 
 export default HeroSection;
