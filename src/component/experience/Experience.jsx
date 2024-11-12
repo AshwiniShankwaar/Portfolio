63c05f20d364c61fd09aaa90c5dfa6a6d2526277
ashwinishankwaar <ashwinishankwaar@gmail.com>
2024-11-13 01:25:17 +0530

    experience component is added and imported in app.js file

---
+++ src/component/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import Style from './experience.module.css';
+
+function Experience (){
+    return(
+        <div>
+            experience section
+        </div>
+    )
+}
+export default Experience;
